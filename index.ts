8987bacd395164de8cbd98bbe993ed1119fbf491
Kex1016 <cakes@haiiro.moe>
2024-01-02 20:29:22 +0100

    Added API to the bot

--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
 import "dotenv/config";
-import { dfp } from "./utils/dfp.js";
-import { client } from "./utils/discord.js";
+import { dfp } from "@/utils/dfp.js";
+import { client } from "@/utils/discord.js";
+import logging from "@/utils/logging.js";
+import app from "@/utils/api.js";
 
 //store your token in environment variable or put it here
 const token = process.env["TOKEN"];
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
+  logging.log(logging.Severity.INFO, `Logged in as ${client.user?.tag}!`);
 
-    dfp.start({
-        client,
-        load: ["./commands"],
-    });
+  dfp.start({
+    client,
+    load: ["./commands"],
+  });
+
+  app.listen(process.env.API_PORT || 3000, () => {
+    logging.log(
+      logging.Severity.INFO,
+      `API listening on port ${process.env.API_PORT || 3000}`
+    );
+  });
 });
 
 client.login(token);
