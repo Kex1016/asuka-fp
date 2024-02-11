1e6022660b46e1401072c719402490190d64dab9
Kex (David) <nyilasdavid@gmail.com>
2024-02-11 12:53:57 +0100

    Randomize the users in pairing

--- commands/exchange/forcepair.ts
+++ commands/exchange/forcepair.ts
@@ -55,6 +55,12 @@ export async function pairExchangeMembers(
     );
   })) as ExchangeUserType[];
 
+  // Jumble up the users
+  for (let i = users.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [users[i], users[j]] = [users[j], users[i]];
+  }
+
   // -- Pair the users --
   // Pairing steps:
   // - Get two unpaired users and pair them
